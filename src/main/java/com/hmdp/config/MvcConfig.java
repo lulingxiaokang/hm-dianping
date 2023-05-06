c1c34bcb06404b1b4fe9d30c5726f3933df69b9f
llxk <1820472818@qq.com>
2023-05-06 23:18:55 +0800

    短信登陆功能完善（登陆状态刷新）

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.hmdp.config;
 
 import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -24,7 +25,8 @@ public class MvcConfig implements WebMvcConfigurer {
     private StringRedisTemplate stringRedisTemplate;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate))
+        //登陆拦截器
+        registry.addInterceptor(new LoginInterceptor())
                         .excludePathPatterns(
                           "/user/login",
                           "/user/code",
@@ -33,7 +35,10 @@ public class MvcConfig implements WebMvcConfigurer {
                           "/shop-type/**",
                           "/upload/**",
                           "/voucher/**"
-                        );
+                        ).order(1);
+        //token刷新拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate))
+                .addPathPatterns("/**").order(0);
     }
 
 }
